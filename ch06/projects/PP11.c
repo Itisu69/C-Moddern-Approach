f630575bd2f47b6420863220c686aa14002727b3
Itisu69 <hpstudent33@gmail.com>
2026-09-18 17:12:34 +0530

    Finished Project 11

--- ch06/projects/PP11.c
+++ ch06/projects/PP11.c
@@ -1,7 +1,24 @@
-#include <stdio.h>
 
 // Calculates the approximate value of e
+
+#include <stdio.h>
+
 int main()
 {
-    
+	int n = 0;
+	float e = 1.0f;
+	float factorial = 1.0f;
+
+	// Taking an input of n
+	printf("Enter a number: ");
+	scanf("%d", &n);
+
+	// Calculating approximate value
+	for(int i = 1; i <= n; i++){
+		factorial *= i;
+		e += 1.0f / factorial;
+	}
+	printf("Approximate value of e: %f\n", e);
+
+	return 0;
 }
