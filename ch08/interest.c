91ec6831d785ebb7e7da34a0accd02be5f508984
Itisu69 <hpstudent33@gmail.com>
2026-09-20 17:29:22 +0530

    Formatted

--- ch08/interest.c
+++ ch08/interest.c
@@ -2,13 +2,12 @@
 
 #include <stdio.h>
 
-#define NUM_RATES ((int)(sizeof (value) / sizeof(value[0])))
+#define NUM_RATES ((int)(sizeof(value) / sizeof(value[0])))
 #define INITIAL_BALANCE 100.00
 
 #include <stdio.h>
 
-int main()
-{
+int main() {
     double value[5];
     int low_rate, i, num_years, year;
 
@@ -19,18 +18,17 @@ int main()
     scanf("%d", &num_years);
 
     printf("\nYears");
-    for (int i = 0; i < NUM_RATES; i++) // this loop assigns $100 to every interest rate for intializing
+    for (int i = 0; i < NUM_RATES;
+         i++) // this loop assigns $100 to every interest rate for intializing
     {
         printf(" %6d%%", low_rate + i);
         value[i] = INITIAL_BALANCE;
     }
     printf("\n");
 
-    for (int year = 1; year <= num_years; year++)
-    {
+    for (int year = 1; year <= num_years; year++) {
         printf("%3d     ", year);
-        for (int i = 0; i < NUM_RATES; i++)
-        {
+        for (int i = 0; i < NUM_RATES; i++) {
             value[i] = value[i] + ((low_rate + i) / 100.0) * value[i];
             printf(" %7.2f", value[i]);
         }
