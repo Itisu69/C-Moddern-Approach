852808303ae20ed99f36fee045aa24eba492b906
Itisu69 <hpstudent33@gmail.com>
2026-09-20 16:32:28 +0530

    Original

--- ch08/reverse.c
+++ ch08/reverse.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-#define ARRAY_LENGTH ((int) (sizeof(a)/sizeof(a[0])))
+#define N 10
 
-int main()
-{
-    int a[10], i;
-    printf("Enter 10 integers: ");
-    for (int i = 0; i < ARRAY_LENGTH; i++)
-    {
-        scanf("%d", &a[i]);
-    }
+int main() {
+  int a[N], i;
+  printf("Enter %d integers: ", N);
+  for (int i = 0; i < N; i++) {
+    scanf("%d", &a[i]);
+  }
 
-    printf("In Reverse order: ");
-    for (int i = ARRAY_LENGTH - 1; i >= 0; i--)
-    {
-        printf(" %d", a[i]);
-    }
-    printf("\n");
+  printf("In Reverse order: ");
+  for (int i = N - 1; i >= 0; i--) {
+    printf(" %d", a[i]);
+  }
+  printf("\n");
 
-    return 0;
+  return 0;
 }
