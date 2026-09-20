e51d27e9ae213fff2235a2fdf73728bebccfaae0
Itisu69 <hpstudent33@gmail.com>
2026-09-20 10:31:07 +0530

    Formatted file

--- ch08/projects/PP01.c
+++ ch08/projects/PP01.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int no_of_digits_seen[10] = {0};
-    int digit;
-    long n;
+int main(void) {
+  int no_of_digits_seen[10] = {0};
+  int digit;
+  long n;
 
-    printf("Enter a number: ");
-    scanf("%ld", &n);
+  printf("Enter a number: ");
+  scanf("%ld", &n);
 
-    while (n > 0)
-    {
-        digit = n % 10;
-        no_of_digits_seen[digit]++;
-        n /= 10;
-    }
+  while (n > 0) {
+    digit = n % 10;
+    no_of_digits_seen[digit]++;
+    n /= 10;
+  }
 
-    printf("Repeated digit(s): ");
-    for (n = 0; n < 10; n++)
-    {
-        if (no_of_digits_seen[n] > 1)
-            printf("%ld ", n);
-    }
+  printf("Repeated digit(s): ");
+  for (n = 0; n < 10; n++) {
+    if (no_of_digits_seen[n] > 1)
+      printf("%ld ", n);
+  }
 
-    return 0;
+  return 0;
 }
