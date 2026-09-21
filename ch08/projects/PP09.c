fd7666b886a4a028812b27b3df3da807608c304d
Itisu69 <hpstudent33@gmail.com>
2026-09-21 16:21:29 +0530

    Formatted

--- ch08/projects/PP09.c
+++ ch08/projects/PP09.c
@@ -1,7 +1,7 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
 
 #define NUM_ROWS 10
 #define NUM_COLS 10
@@ -14,46 +14,42 @@
 #define RIGHT 3
 
 int main() {
-    char grid[NUM_ROWS][NUM_COLS],letter;
+    char grid[NUM_ROWS][NUM_COLS], letter;
     bool pot_move[NUM_MOVES];
-    int i,row,col,pot_move_count,gen_move;
+    int i, row, col, pot_move_count, gen_move;
 
-    srand((unsigned) time (NULL));
+    srand((unsigned)time(NULL));
 
-    //Filling the grid with '.'
-    for ( row = 0; row < NUM_ROWS; row++)
-    {
-        for ( col = 0; col < NUM_COLS; col++)
-        {
+    // Filling the grid with '.'
+    for (row = 0; row < NUM_ROWS; row++) {
+        for (col = 0; col < NUM_COLS; col++) {
             grid[row][col] = '.';
         }
-        
     }
-    
-    row = 0; col=0;
-    for ( letter = 'A'; letter <= 'Z'; letter++)
-    {
-        grid[row][col]=letter;
-        
-        for ( i = 0; i < NUM_MOVES; pot_move[i]=false,i++)
-        {
-            ; 
-        }
-        pot_move_count=0;
 
-        if (col-1 >= 0 && grid[row][col-1] == EMPTY_CELL) {
+    row = 0;
+    col = 0;
+    for (letter = 'A'; letter <= 'Z'; letter++) {
+        grid[row][col] = letter;
+
+        for (i = 0; i < NUM_MOVES; pot_move[i] = false, i++) {
+            ;
+        }
+        pot_move_count = 0;
+
+        if (col - 1 >= 0 && grid[row][col - 1] == EMPTY_CELL) {
             pot_move[LEFT] = true;
             pot_move_count++;
         }
-        if (row+1 < NUM_ROWS && grid[row+1][col] == EMPTY_CELL) {
+        if (row + 1 < NUM_ROWS && grid[row + 1][col] == EMPTY_CELL) {
             pot_move[DOWN] = true;
             pot_move_count++;
         }
-        if (row-1 >= 0 && grid[row-1][col] == EMPTY_CELL) {
+        if (row - 1 >= 0 && grid[row - 1][col] == EMPTY_CELL) {
             pot_move[UP] = true;
             pot_move_count++;
         }
-        if (col+1 < NUM_COLS && grid[row][col+1] == EMPTY_CELL) {
+        if (col + 1 < NUM_COLS && grid[row][col + 1] == EMPTY_CELL) {
             pot_move[RIGHT] = true;
             pot_move_count++;
         }
@@ -62,29 +58,33 @@ int main() {
         if (pot_move_count == 0)
             break;
 
-        /* Generate random moves until one matches a potential move, then perform 
-         * move */
+        /* Generate random moves until one matches a potential move, then
+         * perform move */
         while (true) {
             gen_move = rand() % NUM_MOVES;
 
             if (pot_move[gen_move] == true) {
-                switch(gen_move) {
-                    case LEFT:  col--;
-                                break;
-                    case DOWN:  row++;
-                                break;
-                    case UP:    row--;
-                                break;
-                    case RIGHT: col++;
-                                break;
+                switch (gen_move) {
+                case LEFT:
+                    col--;
+                    break;
+                case DOWN:
+                    row++;
+                    break;
+                case UP:
+                    row--;
+                    break;
+                case RIGHT:
+                    col++;
+                    break;
                 }
-                break;    /* generated move performed, break */
+                break; /* generated move performed, break */
             } else {
                 continue; /* generated move not possible, continue */
             }
         }
     }
-             
+
     /* Print the final grid state */
     for (row = 0; row < 10; row++) {
         for (col = 0; col < 10; col++) {
