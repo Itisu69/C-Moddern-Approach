e252114748271a693bbaa4d1e73832ec11869e73
Itisu69 <hpstudent33@gmail.com>
2026-09-21 10:17:25 +0530

    Modified

--- ch08/reverse.c
+++ ch08/reverse.c
@@ -3,17 +3,17 @@
 #define N 10
 
 int main() {
-  int a[N], i;
-  printf("Enter %d integers: ", N);
-  for (int i = 0; i < N; i++) {
-    scanf("%d", &a[i]);
-  }
+    int a[N], i;
+    printf("Enter %d integers: ", N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &a[i]);
+    }
 
-  printf("In Reverse order: ");
-  for (int i = N - 1; i >= 0; i--) {
-    printf(" %d", a[i]);
-  }
-  printf("\n");
-
-  return 0;
+    printf("In Reverse order: ");
+    for (int i = N - 1; i >= 0; i--) {
+        printf(" %d", a[i]);
+    }
+    printf("\n");
+    printf("");
+    return 0;
 }
