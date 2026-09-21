5bc246195dad469a1148bbcd83dff9b7ae6f59bd
Itisu69 <hpstudent33@gmail.com>
2026-09-21 15:42:32 +0530

    Formatted

--- ch08/projects/PP06.c
+++ ch08/projects/PP06.c
@@ -1,32 +1,28 @@
-//Program to print message after BIFF filterering it
+// Program to print message after BIFF filterering it
 
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
-int main()
-{
-    char cha, certain_char[6] = {'A', 'B', 'E', 'I', 'O', 'S'}, message[500], biff[6] = {'4', '8', '3', '1', '0', '5'};
-    int i = 0, j = 0,k =0;
+int main() {
+    char cha, certain_char[6] = {'A', 'B', 'E', 'I', 'O', 'S'}, message[500],
+              biff[6] = {'4', '8', '3', '1', '0', '5'};
+    int i = 0, j = 0, k = 0;
 
     printf("Enter a message: ");
 
-    while (1)
-    {
+    while (1) {
         scanf("%c", &cha);
         cha = toupper(cha);
-        k=0;
-        while (k<6)
-        {
-            if (cha == certain_char[k])
-            {
+        k = 0;
+        while (k < 6) {
+            if (cha == certain_char[k]) {
                 cha = biff[k];
             }
             k++;
         }
-        
+
         message[i] = cha;
-        if (cha == '\n')
-        {
+        if (cha == '\n') {
             break;
         }
         i++;
@@ -34,13 +30,11 @@ int main()
 
     i = 0;
 
-    for (;;)
-    {
+    for (;;) {
 
-       printf("%c",message[j]);
+        printf("%c", message[j]);
         j++;
-        if (message[j] == '\n')
-        {
+        if (message[j] == '\n') {
             printf("!!!!!!!!");
             break;
         }
