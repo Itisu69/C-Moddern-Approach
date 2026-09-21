aa4b427390b0f7380e814cad2194ef0be640ab1e
Itisu69 <hpstudent33@gmail.com>
2026-09-21 16:20:52 +0530

    Fixed the logic of the last loop

--- ch08/projects/PP16.c
+++ ch08/projects/PP16.c
@@ -1,78 +1,67 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-int main()
-{
+int main() {
     char f_word[20] = {0}, s_word[20] = {0};
-    char letters[26] = {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-        'u', 'v', 'w', 'x', 'y', 'z'};
+    char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                        's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    int letters_seen[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0};
+    int letters_seen[26] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     printf("Enter first word: ");
-    for (int i = 0; i < 20; i++)
-    {
+    for (int i = 0; i < 20; i++) {
         scanf("%c", &f_word[i]);
 
         f_word[i] = tolower(f_word[i]);
-        if (f_word[i] == '\n')
-        {
+        if (f_word[i] == '\n') {
             break;
         }
     }
 
     printf("Enter second word: ");
-    for (int i = 0; i < 20; i++)
-    {
+    for (int i = 0; i < 20; i++) {
         scanf("%c", &s_word[i]);
         s_word[i] = tolower(s_word[i]);
-        if (s_word[i] == '\n')
-        {
+        if (s_word[i] == '\n') {
             break;
         }
     }
 
-    for (int i = 0; i < 20; i++)
-    {
-        for (int j = 0; j < 26; j++)
-        {
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 26; j++) {
 
-            if (f_word[i] == letters[j])
-            {
-                if (f_word[i] >= 'a' && f_word[i] <= 'z')
-                {
+            if (f_word[i] == letters[j]) {
+                if (f_word[i] >= 'a' && f_word[i] <= 'z') {
 
                     letters_seen[j] += 1;
-                }
-                else continue;
+                } else
+                    continue;
             }
-            if (s_word[i] == letters[j])
-            {
-                if (s_word[i] >= 'a' && s_word[i] <= 'z')
-                {
+            if (s_word[i] == letters[j]) {
+                if (s_word[i] >= 'a' && s_word[i] <= 'z') {
 
                     letters_seen[j] -= 1;
-                }
-                else continue;
+                } else
+                    continue;
             }
         }
     }
 
-    for (int i = 0; i < 26; i++)
-    {
-        if (letters_seen[i] > 0)
-        {
-            printf("The word is not anagram");
+    bool is_anagram = true;
+    for (int i = 0; i < 26; i++) {
+        if (letters_seen[i] != 0) {
+            is_anagram = false;
             break;
         }
-        else
-            printf("The word is anagram");
-            break;
     }
 
+    if (is_anagram) {
+        printf("The words are anagrams.\n");
+    } else {
+        printf("The words are not anagrams.\n");
+    }
     return 0;
 }
