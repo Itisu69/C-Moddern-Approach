58d01a3c4554d75b0a2d0941d5aba827f4608c80
Itisu69 <hpstudent33@gmail.com>
2026-09-23 14:57:58 +0530

    formatted

--- check_std.c
+++ check_std.c
@@ -3,14 +3,14 @@
 int main() {
 #ifdef __STDC_VERSION__
     long version = __STDC_VERSION__;
-    
-    if (version == 199409L) 
+
+    if (version == 199409L)
         printf("Standard: C94/C95\n");
-    else if (version == 199901L) 
+    else if (version == 199901L)
         printf("Standard: C99\n");
-    else if (version == 201112L) 
+    else if (version == 201112L)
         printf("Standard: C11\n");
-    else if (version == 201710L) 
+    else if (version == 201710L)
         printf("Standard: C17/C18\n");
     else if (version > 201710L)
         printf("Standard: C23 (or newer)\n");
